3f7081ceb8b0fa21f2e50e3ac67f67f65d2cbbb8
Kevin Schaul <kevin.schaul@gmail.com>
2023-10-28 17:25:48 -0500

    benchmark find move function

--- badchess/benchmark/memory/test_memory.py
+++ badchess/benchmark/memory/test_memory.py
@@ -1,6 +1,7 @@
 import chess
 from badchess.main import (
     build_move_tree,
+    find_best_move,
 )
 
 board_init = chess.Board()
@@ -13,3 +14,9 @@ def test_build_move_tree_depth_3():
 
 def test_build_move_tree_depth_4():
     build_move_tree(board_init, depth=4)
+
+def test_find_best_move_depth_3():
+    find_best_move(board_init, depth=3)
+
+def test_find_best_move_depth_4():
+    find_best_move(board_init, depth=4)
