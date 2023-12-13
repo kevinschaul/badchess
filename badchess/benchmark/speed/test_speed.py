baa9ee5f208e28691fb1c6027f8cc0776fce1c80
Kevin Schaul <kevin.schaul@gmail.com>
2023-12-13 11:04:04 -0600

    clean up benchmarks

--- badchess/benchmark/speed/test_speed.py
+++ badchess/benchmark/speed/test_speed.py
@@ -1,30 +1,14 @@
 import chess
 from badchess.main import (
-    build_move_tree,
-    estimate_strength,
     find_best_move,
 )
 
-FEN_MORPHY_DEFENCE = (
-    "r1bqkbnr/1ppp1ppp/p1n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 4"
-)
+fen='r1bqkbnr/1ppp1ppp/p1n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 4'
 
+def test_find_best_move_3(benchmark):
+    board = chess.Board(fen)
+    benchmark(find_best_move, board, 3)
 
-def test_estimate_strength(benchmark):
-    board = chess.Board()
-    benchmark(estimate_strength, board)
-
-
-# def test_build_move_tree(benchmark):
-#     board = chess.Board()
-#     benchmark(build_move_tree, board, depth=2)
-
-
-def test_find_best_move(benchmark):
-    board = chess.Board()
-    benchmark(find_best_move, board)
-
-
-def test_find_best_move_morphy(benchmark):
-    board = chess.Board(FEN_MORPHY_DEFENCE)
-    benchmark(find_best_move, board)
+def test_find_best_move_4(benchmark):
+    board = chess.Board(fen)
+    benchmark(find_best_move, board, 4)
